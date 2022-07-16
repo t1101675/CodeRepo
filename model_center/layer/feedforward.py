969183bb2001c6932385f0d96ea92e09fc9e6322
t1101675 <guyx21@mails.tsinghua.edu.cn>
2022-07-16 12:00:21 +0800

    use F.gelu

--- model_center/layer/feedforward.py
+++ model_center/layer/feedforward.py
@@ -19,14 +19,18 @@ import bmtrain as bmt
 from .linear import Linear
 
 import math
+import torch.nn.functional as F
 
-@torch.jit.script
-def gelu_new(x):
-    """
-    Implementation of the GELU activation function currently in Google BERT repo (identical to OpenAI GPT). Also see
-    the Gaussian Error Linear Units paper: https://arxiv.org/abs/1606.08415
-    """
-    return 0.5 * x * (1.0 + torch.tanh(math.sqrt(2.0 / math.pi) * (x + 0.044715 * torch.pow(x, 3.0))))
+# @torch.jit.script
+# def gelu_new(x):
+#     """
+#     Implementation of the GELU activation function currently in Google BERT repo (identical to OpenAI GPT). Also see
+#     the Gaussian Error Linear Units paper: https://arxiv.org/abs/1606.08415
+#     """
+#     return 0.5 * x * (1.0 + torch.tanh(math.sqrt(2.0 / math.pi) * (x + 0.044715 * torch.pow(x, 3.0))))
+
+
+gelu_new = F.gelu
 
 class DenseGatedACT(bmt.DistributedModule):
 
